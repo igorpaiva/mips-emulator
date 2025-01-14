c09ccfbfefbc909385764a2c2fddfd6206446591
Igor Paiva <igor.araujo.ee@gmail.com>
2025-01-15 00:51:37 +0100

    Refactor tests and fixtures to follow rktest structure. Add test template

--- test/test_instructions.c
+++ test/test_instructions.c
@@ -2,17 +2,18 @@
 #include "memory.h"
 #include "instructions.h"
 
-// Test case: LUI instruction
-TEST(instruction_tests, test_lui) {
+TEST_SETUP(instruction_tests) {
     reset_registers_and_memory();
+}
+
+TEST(instruction_tests, test_lui) {
     int rt = 8; // $t0
     lui(rt, 0x1234);
     EXPECT_EQ(registers[rt], 0x12340000);
 }
 
-// Test case: ORI instruction
+
 TEST(instruction_tests, test_ori) {
-    reset_registers_and_memory();
     int rt = 8; // $t0
     int rs = 9; // $t1
     lui(rs, 0x1234);
@@ -20,9 +21,7 @@ TEST(instruction_tests, test_ori) {
     EXPECT_EQ(registers[rt], 0x12345678);
 }
 
-// Test case: ADDI instruction
 TEST(instruction_tests, test_addi) {
-    reset_registers_and_memory();
     int rt = 8; // $t0
     int rs = 9; // $t1
     registers[rs] = 0x1234;
--- test/test_memory.c
+++ test/test_memory.c
@@ -1,22 +1,17 @@
 #include "rktest/rktest.h"
 #include "memory.h"
 
-// Helper: Setup function
-void setup(void) {
+TEST_SETUP(memory_tests) {
     reset_registers_and_memory();
 }
 
-// Test case: Check if registers are reset
 TEST(memory_tests, reset_registers) {
-    setup();
     for (int i = 0; i < 32; i++) {
         EXPECT_EQ(registers[i], 0);
     }
 }
 
-// Test case: Check if memory is reset
 TEST(memory_tests, reset_memory) {
-    setup();
     for (int i = 0; i < MEMORY_SIZE; i++) {
         EXPECT_EQ(memory[i], 0);
     }
